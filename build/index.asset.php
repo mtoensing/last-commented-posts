b79a6d4a7337010960d5a7569b9380f4066b4343
Marc Toensing <marc.toensing@gmail.com>
2021-12-05 14:48:55 +0100

    added build

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '4f5f0118ef5092057b0373fe25484fec');
