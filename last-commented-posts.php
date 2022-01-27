262242e075f4266082889d0efd68b239085d4ab5
Marc Toensing <marc.toensing@gmail.com>
2022-01-27 23:04:24 +0100

    Links are not clickable in editor

--- last-commented-posts.php
+++ last-commented-posts.php
@@ -3,7 +3,7 @@
  * Plugin Name: Last Commented Posts Block
  * Plugin URI: https://marc.tv/
  * Description: Adds a block that lists the recent commented posts.
- * Version: 1.6
+ * Version: 1.7
  * Author: Marc Tönsing
  * Author URI: https://marc.tv
  * Text Domain: lastcommentedposts
@@ -119,6 +119,8 @@ function query_posts_with_recent_comments($limit)
         $align = $attributes['align'];
         $icon = '<svg style="height: 0.75em; padding-right: 4px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><rect x="0" fill="none" width="20" height="20"/><g><path d="M10 9.25c-2.27 0-2.73-3.44-2.73-3.44C7 4.02 7.82 2 9.97 2c2.16 0 2.98 2.02 2.71 3.81 0 0-.41 3.44-2.68 3.44zm0 2.57L12.72 10c2.39 0 4.52 2.33 4.52 4.53v2.49s-3.65 1.13-7.24 1.13c-3.65 0-7.24-1.13-7.24-1.13v-2.49c0-2.25 1.94-4.48 4.47-4.48z"/></g></svg>';
         $alignclass = '';
+        $is_backend = defined('REST_REQUEST') && true === REST_REQUEST && 'edit' === filter_input(INPUT_GET, 'context', FILTER_SANITIZE_STRING);
+  
 
         if ($align != '') {
           $alignclass = 'align' . $align;
@@ -137,11 +139,13 @@ function query_posts_with_recent_comments($limit)
             //echo "<!-- Elapsed time is: ". (microtime(true) - $startTime) ." seconds -->";
             $authorname = $comment->comment_author;
 
-            if (strlen($authorname) > 20) {
-                $authorname = substr($authorname, 0, 18) . '...';
+            $url = $comment_url;
+
+            if ($is_backend) {
+              $url = '#';
             }
             
-            $comment_user = '<a href="' . $comment_url . '">' .  get_the_title($result->ID) . '<br><span class="comment-author-link">' . $icon . $authorname . '</span></a>';
+            $comment_user = '<a href="' . $url . '">' .  get_the_title($result->ID) . '<br><span class="comment-author-link">' . $icon . $authorname . '</span></a>';
 
             $html .= $comment_user;
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: Gutenberg, block, comments,
 Requires at least: 5.0
 Donate link: https://marc.tv/out/donate
 Tested up to: 5.8
-Stable tag: 1.6
+Stable tag: 1.7
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
