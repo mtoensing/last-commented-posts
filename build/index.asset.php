917c691fcf5e258103c0e1bd36867e53b8749fc4
Marc Toensing <marc.toensing@gmail.com>
2023-03-05 17:00:24 +0100

    new build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '789a86a974700f9df9f0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '7e378ffeca9b7f482519');
